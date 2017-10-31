f49dd5be2b90e9727250bbdd13aa95348414e978
Komaricus <33253356+Komaricus@users.noreply.github.com>
2017-10-31 14:34:59 +0300

    test test test

--- CountOnLight/Program.cs
+++ CountOnLight/Program.cs
@@ -7,6 +7,8 @@ namespace CountOnLight
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
         }
     }
 }
